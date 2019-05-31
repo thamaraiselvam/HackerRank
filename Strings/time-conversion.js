2d336703b748c054e1ad9d59c22a189aeb85c42d
Thamaraiselvam <thamaraiselvam@live.com>
2019-05-31 17:27:30 +0530

    Create time-conversion.js

---
+++ Strings/time-conversion.js
@@ -0,0 +1,30 @@
+/*
+ Problem name: Time Conversion
+ Difficulty: Easy
+ Score: 15
+ Link: https://www.hackerrank.com/challenges/time-conversion/
+*/
+
+
+function timeConversion(time) {
+    let cycle = time.substring(time.length - 2, time.length);
+    let timeWithoutCycle = time.substring(0, time.length - 2);
+    let timeArr = timeWithoutCycle.split(':');
+
+    if (cycle === 'AM') {
+        if (timeArr[0] != 12) {
+            return timeWithoutCycle;
+        }
+
+        timeArr[0] = '00';
+        return timeArr.join(':');
+    }
+
+    if (timeArr[0] != 12) {
+        timeArr[0] = parseInt(timeArr[0]) + 12;
+    }
+
+    return timeArr.join(':')
+}
+
+console.log("07:05:45PM");
