07640e8876a3697d4cb9feba0fde2906a92e5be6
Thamaraiselvam <thamaraiselvam@live.com>
2019-05-31 17:24:51 +0530

    Create birthday-cake-candles.js

---
+++ Arrays/birthday-cake-candles.js
@@ -0,0 +1,23 @@
+/*
+ Problem name: Birthday Cake Candles
+ Difficulty: Easy
+ Score: 10
+ Link: https://www.hackerrank.com/challenges/birthday-cake-candles/
+*/
+
+function birthdayCakeCandles(arr) {
+    let tallestCandle = 0;
+    let candlesCounter = 0;
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] > tallestCandle) {
+            tallestCandle = arr[index];
+            candlesCounter = 1;
+        } else if (arr[index] === tallestCandle) {
+            candlesCounter++;
+        }
+    }
+
+    return candlesCounter;
+}
+
+console.log(birthdayCakeCandles([3,2,1,3]));
