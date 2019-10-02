42a2a1ccccce036555ff514d00e6fc05c99ff6b1
Tobias Lehwalder <tlehwalder@posteo.de>
2019-10-02 19:23:29 +0200

    adds test cases for Repeated String and exports repeated string function

--- Strings/repeated-string.js
+++ Strings/repeated-string.js
@@ -5,7 +5,7 @@
  Link: https://www.hackerrank.com/challenges/repeated-string
 */
 
-function repeatedString(s, n) {
+module.exports = function repeatedString(s, n) {
     let initialCount = s.split('a').length - 1;
     let cocent = Math.floor(n / s.length);
     let total = cocent * initialCount;
