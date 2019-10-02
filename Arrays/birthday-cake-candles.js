23b7ccec5eeb8de08e87c4345b5b325e5fc8697f
Tobias Lehwalder <tlehwalder@posteo.de>
2019-10-03 00:27:28 +0200

    Adds test coverage for Birthday Cake Candles and exports birthdayCakeCandles function

--- Arrays/birthday-cake-candles.js
+++ Arrays/birthday-cake-candles.js
@@ -5,19 +5,17 @@
  Link: https://www.hackerrank.com/challenges/birthday-cake-candles/
 */
 
-function birthdayCakeCandles(arr) {
-    let tallestCandle = 0;
-    let candlesCounter = 0;
-    for (let index = 0; index < arr.length; index++) {
-        if (arr[index] > tallestCandle) {
-            tallestCandle = arr[index];
-            candlesCounter = 1;
-        } else if (arr[index] === tallestCandle) {
-            candlesCounter++;
-        }
+module.exports = function birthdayCakeCandles(arr) {
+  let tallestCandle = 0;
+  let candlesCounter = 0;
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] > tallestCandle) {
+      tallestCandle = arr[index];
+      candlesCounter = 1;
+    } else if (arr[index] === tallestCandle) {
+      candlesCounter++;
     }
+  }
 
-    return candlesCounter;
+  return candlesCounter;
 }
-
-console.log(birthdayCakeCandles([3,2,1,3]));
---
+++ test/Arrays/birthday-cake-candles.test.js
@@ -0,0 +1,28 @@
+const birthdayCakeCandles = require("../../Arrays/birthday-cake-candles");
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
+
+describe("Birthday Cake Candles", () => {
+  it("returns 0 for empty array", () => {
+    expect(birthdayCakeCandles([])).to.equal(0);
+  });
+
+  it("returns 1 if there is only one candle", () => {
+    expect(birthdayCakeCandles([1])).to.equal(1);
+    expect(birthdayCakeCandles([2])).to.equal(1);
+    expect(birthdayCakeCandles([3])).to.equal(1);
+  });
+
+  it("returns 2 if there are two candles with the same height", () => {
+    expect(birthdayCakeCandles([1, 1])).to.equal(2);
+    expect(birthdayCakeCandles([2, 2])).to.equal(2);
+    expect(birthdayCakeCandles([3, 3])).to.equal(2);
+  });
+
+  it("returns correct amount for array with different sized candles", () => {
+    expect(birthdayCakeCandles([3, 2, 1, 3])).to.equal(2);
+    expect(birthdayCakeCandles([3, 2, 1, 3, 4, 5, 4])).to.equal(1);
+    expect(birthdayCakeCandles([3, 2, 1, 1, 2, 2, 4, 4, 4])).to.equal(3);
+    expect(birthdayCakeCandles([3, 2, 1])).to.equal(1);
+  });
+});
